c48843b28601ca57f0c15c5113629438ebfcc8cf
Gautam Raghu <graghu@purdue.edu>
2019-09-14 14:50:30 -0400

    crosshair addition

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,7 +3,7 @@ using System;
 
 public class Player : Area2D
 {
-	public const int SPEED = -270;
+    public const int SPEED = -270;
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
@@ -11,23 +11,28 @@ public class Player : Area2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        var arrow = ResourceLoader.Load("res://arrow.png");
+        Input.SetCustomMouseCursor(arrow);
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
- public override void _Process(float delta)
-{
-  if (Input.IsActionPressed("ui_up")){
-	MoveLocalY(SPEED*delta);
- }
-if (Input.IsActionPressed("ui_down")){
-	MoveLocalY(-SPEED*delta);
- }
-if (Input.IsActionPressed("ui_right")){
-	MoveLocalX(-SPEED*delta);
- }
-if (Input.IsActionPressed("ui_left")){
-	MoveLocalX(SPEED*delta);
- }
-}
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+      if (Input.IsActionPressed("ui_up"))
+      {
+          MoveLocalY(SPEED * delta);
+      }
+      if (Input.IsActionPressed("ui_down"))
+      {
+          MoveLocalY(-SPEED * delta);
+      }
+      if (Input.IsActionPressed("ui_right"))
+      {
+          MoveLocalX(-SPEED * delta);
+      }
+      if (Input.IsActionPressed("ui_left"))
+      {
+          MoveLocalX(SPEED * delta);
+      }
+    }
 }
