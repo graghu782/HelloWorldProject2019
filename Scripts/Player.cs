3f3a597f3a2995110629e049965820039ca1695c
Jorge Ribeiro <ribeiroj@purdue.edu>
2019-09-14 14:31:58 -0400

    Character Movement

---
+++ Scripts/Player.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public class Player : Area2D
+{
+	public const int SPEED = -270;
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+ public override void _Process(float delta)
+{
+  if (Input.IsActionPressed("ui_up")){
+	MoveLocalY(SPEED*delta);
+ }
+if (Input.IsActionPressed("ui_down")){
+	MoveLocalY(-SPEED*delta);
+ }
+if (Input.IsActionPressed("ui_right")){
+	MoveLocalX(-SPEED*delta);
+ }
+if (Input.IsActionPressed("ui_left")){
+	MoveLocalX(SPEED*delta);
+ }
+}
+}
